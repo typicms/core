b4aca403e10e045dca93cb60f76822a22bf1ff4a
Samuel De Backer <sdebacker@gmail.com>
2026-04-30 13:08:26 +0200

    Update public.js

--- resources/js/public.js
+++ resources/js/public.js
@@ -1,35 +1,24 @@
-/**
- * Bootstrap
- */
 import Alert from 'bootstrap/js/dist/alert';
 import Collapse from 'bootstrap/js/dist/collapse';
 import Dropdown from 'bootstrap/js/dist/dropdown';
-
-/**
- * Swiper
- */
 import Swiper from 'swiper';
 import { Autoplay, EffectFade, Navigation, Pagination, Parallax } from 'swiper/modules';
-
-Swiper.use([Navigation, Pagination, Autoplay, Parallax, EffectFade]);
-window.Swiper = Swiper;
 
 import enableAnchorTop from './public/anchor-top.ts';
 import enableNavigation from './public/navigation.ts';
 import enablePhotoSwipeLightbox from './public/photo-swipe-lightbox.ts';
+
+import.meta.glob(['../images/**'], { eager: true, query: '?url' });
+
+Swiper.use([Navigation, Pagination, Autoplay, Parallax, EffectFade]);
+window.Swiper = Swiper;
 
 enablePhotoSwipeLightbox();
 enableAnchorTop();
 enableNavigation();
 
 /**
- * Images
- */
-import.meta.glob(['../images/**'], { eager: true, query: '?url' });
-
-/**
  * For TypiCMS’s Places module
  */
 // import initMap from './public/map';
 // window.initMap = initMap;
-
