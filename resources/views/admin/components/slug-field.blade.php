a2da257845396a9527559c1d85dd97875f05f870
Samuel De Backer <sdebacker@gmail.com>
2026-05-05 18:34:15 +0200

    Update slug-field.blade.php

--- resources/views/admin/components/slug-field.blade.php
+++ resources/views/admin/components/slug-field.blade.php
@@ -1,24 +1,11 @@
 @props(['locales' => locales()])
 
 @foreach ($locales as $locale)
-    <div class="mb-3 form-group-translation @if ($errors->has('slug.' . $locale)) has-error @endif">
-        {!!
-            Form::label('<span>' . __('Slug') . '</span> <span>(' . $locale . ')</span>')
-                ->addClass('form-label')
-                ->forId('slug[' . $locale . ']')
-        !!}
-        <span></span>
-        <div class="input-group">
-            {!!
-                Form::text('slug[' . $locale . ']')
-                    ->addClass('form-control')
-                    ->addClass($errors->has('slug.' . $locale) ? 'is-invalid' : '')
-                    ->id('slug[' . $locale . ']')
-                    ->data('slug', 'title[' . $locale . ']')
-                    ->data('language', $locale)
-            !!}
-            <button class="btn btn-outline-secondary btn-slug" type="button">{{ __('Generate') }}</button>
-            {!! $errors->first('slug.' . $locale, '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-    </div>
+    {!!
+        BootForm::inputGroup(__('Slug') . ' <span>(' . $locale . ')</span>', 'slug[' . $locale . ']')
+            ->attribute('data-slug', 'title[' . $locale . ']')
+            ->data('language', $locale)
+            ->addGroupClass('form-group-translation')
+            ->afterAddon(BootForm::button(__('Generate'))->class('btn btn-outline-secondary btn-slug'))
+    !!}
 @endforeach
