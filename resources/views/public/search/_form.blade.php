a3802f276041ba45ca676b34b071e0a85d5d7b87
Samuel De Backer <sdebacker@gmail.com>
2026-05-26 18:35:00 +0200

    Search form as a modal

--- resources/views/public/search/_form.blade.php
+++ resources/views/public/search/_form.blade.php
@@ -1,8 +1,33 @@
 @if (Route::has(app()->getLocale() . '::search'))
-    <form class="search-form" method="get" action="{{ route(app()->getLocale() . '::search') }}">
-        <div class="input-group">
-            <input class="search-input form-control" type="text" name="query" id="query" aria-label="{{ __('Search') }}" placeholder="{{ __('Search') }}" value="{{ request()->string('query') }}" />
-            <button class="search-button btn btn-primary" type="submit">Search</button>
+    <div class="modal fade modal-xl search-modal" id="search-modal" data-bs-backdrop="static" aria-hidden="true" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="container">
+                <div class="modal-content search-modal-content">
+                    <form class="search-form" method="get" action="{{ route(app()->getLocale() . '::search') }}">
+                        <div class="input-group input-group-lg">
+                            <input
+                                class="search-form-input form-control"
+                                type="text"
+                                name="query"
+                                id="search-modal-input"
+                                aria-label="@lang('Type here to search')"
+                                placeholder="@lang('Type here to search')"
+                                value="{{ request('query') }}"
+                            />
+                            <button class="search-form-button btn btn-primary" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    </form>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <script>
+        const myModal = document.getElementById('search-modal');
+        const myInput = document.getElementById('search-modal-input');
+
+        myModal.addEventListener('shown.bs.modal', () => {
+            myInput.focus();
+        });
+    </script>
 @endif
